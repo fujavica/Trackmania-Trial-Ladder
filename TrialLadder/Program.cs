dcf8ab04a9e1b377827ccb13825467f74df7a147
fujavica <32094225+fujavica@users.noreply.github.com>
2018-12-30 18:22:11 +0100

    Added map info by calling TMX API with locally stored map UIDs

--- TrialLadder/Program.cs
+++ TrialLadder/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Net;
 
 namespace TrialLadder
 {
@@ -14,7 +15,33 @@ namespace TrialLadder
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            /* string filepath = Directory.GetCurrentDirectory();
+             DirectoryInfo d = new DirectoryInfo(filepath);
+
+             foreach (var file in d.GetFiles("*.Challenge.Gbx"))
+             {
+                 Directory.Move(file.FullName, filepath + "\\TextFiles\\" + file.Name);
+             }
+             Map m = new Map();*/
+
+            List<Map> maps = new List<Map>();
+            string path = Path.Combine(Directory.GetCurrentDirectory() + "/maps/maps.txt");
+            var lines = File.ReadLines(path);
+            using (WebClient wc = new WebClient())
+            {
+                foreach (string uid in lines)
+                {
+                    string tmxinfo = wc.DownloadString("http://tmnforever.tm-exchange.com/apiget.aspx?action=apitrackinfo&uid=" + uid );
+                    Map m = new Map();
+                    m.uid = uid; 
+                    m.name = tmxinfo.Split("\t")[1];
+                    m.author = tmxinfo.Split("\t")[3];
+                    maps.Add(m);
+                }
+            }
+            ;
+
+        CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
---
+++ TrialLadder/data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TrialLadder
+{
+    public class Map
+    {
+        public Map()
+        {
+
+        }
+
+        public string name { get; set; }
+        public string uid { get; set; }
+        public string author { get; set; }
+
+    }
+}
